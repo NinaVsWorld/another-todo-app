1446d79553f347ffe1407cd81ce41a1ae0a25c52
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-09-17 20:54:14 +1000

    restart  project. laid out very basic task obj

---
+++ task.js
@@ -0,0 +1,17 @@
+// task object
+class task {
+    constructor(title, description, date, priority, id, projID) {
+        this.title = title
+        this.description = description
+        this.date = date
+        this.priority = priority
+        this.id = id
+        this.projectID = projID
+    }
+};
+
+// create task
+
+// edit task
+
+// delete task
