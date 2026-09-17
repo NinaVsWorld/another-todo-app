f2bf8b9c4a06d3b621e922e2e7fe3f2138431514
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-09-17 21:15:47 +1000

    code deleteTask function:

--- task.js
+++ task.js
@@ -7,13 +7,30 @@ class task {
         this.description = description
         this.date = date
         this.priority = priority
-        this.id = getID() // need to write this function
+        this.id = nextID() // need to write this function
         this.projectID = projID
+    }
+
+    getTaskID() {
+        this.id
     }
 };
 
 // create task
+export function createTask(title, description, date, priority, projID) {
+    const task = task(title, description, date, priority, projID);
+    allTasks.push(task);
+}
 
 // edit task
 
 // delete task
+export function deleteTask(id) {
+    const index = allTasks.findIndex(task => task.getTaskID() == id);
+    if (index > -1) {
+        allTasks.splice(index, 1);
+    }
+
+    // for testing
+    console.log(allTasks);
+}
